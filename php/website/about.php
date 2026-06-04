8e7bc3e752d6310f6e5ce00d982374f987ecabf5
pwad71 <ccsl61588@gmail.com>
2026-06-04 13:06:20 +0600

    convert to php website

---
+++ php/website/about.php
@@ -0,0 +1,10 @@
+<?php 
+ include_once "layout/header.php";
+
+?>
+     <h1>About Page</h1>
+
+     <?php 
+ include_once "layout/footer.php";
+
+?>
---
+++ php/website/service.php
@@ -0,0 +1,7 @@
+<?php 
+ include_once "layout/header.php";
+?>
+<h1>service Page</h1>
+<?php 
+ include_once "layout/footer.php";
+?>
